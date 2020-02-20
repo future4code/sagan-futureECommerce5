a003d9ff40fa538e80b2e50b7f27bfc93ca15a70
Nauara Melo <nauaramelo@gmail.com>
2020-02-20 16:16:31 -0300

    Adicionando forma de filtrar

--- ecommerce5/src/App.js
+++ ecommerce5/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home';
+import Filtro from './components/Filtros';
 
 class App extends React.Component {
   constructor(props) {
@@ -8,76 +9,73 @@ class App extends React.Component {
     this.state = {
       cards: [{
         id: 1,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
-        imageUrl: "https://picsum.photos/200/200"
+        name: "Produto 1",
+        value: 10.00,
+        imageUrl: "https://img.vixdata.io/pd/webp-large/pt/sites/default/files/bbr/nasa1.jpg"
       },
       {
         id: 2,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
+        name: "Produto 12",
+        value: 5.0,
         imageUrl: "https://picsum.photos/200/200"
       },
       {
         id: 3,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
+        name: "Produto 123",
+        value: 6.0,
         imageUrl: "https://picsum.photos/200/200"
       },
       {
         id: 4,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
+        name: "Produto 13",
+        value: 8.0,
         imageUrl: "https://picsum.photos/200/200"
       },
       {
         id: 5,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
+        name: "Produto 16",
+        value: 7.0,
         imageUrl: "https://picsum.photos/200/200"
       },
       {
         id: 6,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
+        name: "Produto 18",
+        value: 2.0,
         imageUrl: "https://picsum.photos/200/200"
       }, {
         id: 7,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
+        name: "Produto 199",
+        value: 1.0,
         imageUrl: "https://picsum.photos/200/200"
       },
       {
         id: 8,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
+        name: "Produto 1222",
+        value: 3.0,
         imageUrl: "https://picsum.photos/200/200"
       },
       {
         id: 9,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
-        imageUrl: "https://picsum.photos/200/200"
-      },
-      {
-        id: 10,
-        name: "Foguete da Missão Apollo 11",
-        value: 10000.0,
+        name: "Produto 134",
+        value: 4.0,
         imageUrl: "https://picsum.photos/200/200"
       }],
+      cardsFiltrados: []
     }
    
+  }
+
+  modificarDadosFiltrados = (novosDadosFiltrados) => {
+    this.setState({
+      cardsFiltrados: novosDadosFiltrados
+    })
   }
   
   render() {
     return (
       <div>
-        <Home imagem = {this.state.cards.map(el =>{
-          return el.imageUrl
-        })} />
-        
-          
-        })} 
+        <Filtro dados = { this.state.cards } modificarDadosFiltrados = {this.modificarDadosFiltrados}  />
+        <Home dados={ this.state.cardsFiltrados.length > 0 ? this.state.cardsFiltrados : this.state.cards} />
       </div>
     )
   }
