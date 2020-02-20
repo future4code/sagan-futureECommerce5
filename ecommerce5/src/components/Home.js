18e98aeb49bff864158f6608a05381b44dc1e531
Nauara Melo <nauaramelo@gmail.com>
2020-02-20 16:18:05 -0300

    Modificando componente para receber o array de dados e renderizar em cima do mesmo

--- ecommerce5/src/components/Home.js
+++ ecommerce5/src/components/Home.js
@@ -4,16 +4,9 @@ import Cards from './Cards'
 function Home(props){
     return (
         <div>
-        <Cards imagem={props.imagem} titulo={''}/>
-        <Cards imagem={require('../img/nasa2.jpg')} titulo="teste"/>
-        <Cards imagem={require('../img/nasa3.jpg')} titulo="teste"/>
-        <Cards imagem={require('../img/nasa4.jpg')} titulo="teste"/>
-        <Cards imagem={require('../img/nasa5.jpg')} titulo="teste"/>
-        <Cards imagem={require('../img/nasa6.jpg')} titulo="teste"/>
-        <Cards imagem={require('../img/nasa7.jpg')} titulo="teste"/>
-        <Cards imagem={require('../img/nasa8.jpg')} titulo="teste"/>
-        <Cards imagem={require('../img/nasa9.jpg')} titulo="teste"/>
-        <Cards imagem={require('../img/nasa11.jpg')} titulo="teste"/>
+            { props.dados.map((dado) => {
+                return <Cards imagem={dado.imageUrl} titulo={dado.name}/>
+            }) }
         </div>
 )
     
